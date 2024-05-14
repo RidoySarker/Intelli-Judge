ff061da7462976d4e136e0baf71dc0cbde77a1f5
MD. Shahidul Bhuiyan <shawon@skylarksoft.com>
2024-05-14 22:57:22 +0600

    fix: some ui in admin panel

--- admin-panel/src/components/layouts/AppsMenu/Main.jsx
+++ admin-panel/src/components/layouts/AppsMenu/Main.jsx
@@ -14,26 +14,26 @@ const Main = () => {
                     </div>
                     <ul className="nav flex-column">
 
-                        <li className="nav-item">
-                            <Link className={location.pathname === "/course-categories" ? 'nav-link' : 'nav-link-disable'}
-                                  to="/course-categories">Course Categories</Link>
-                        </li>
+                        {/*<li className="nav-item">*/}
+                        {/*    <Link className={location.pathname === "/course-categories" ? 'nav-link' : 'nav-link-disable'}*/}
+                        {/*          to="/course-categories">Course Categories</Link>*/}
+                        {/*</li>*/}
 
-                        <li className="nav-item">
-                            <Link className={location.pathname === "/courses" ? 'nav-link' : 'nav-link-disable'} to="/courses">Courses</Link>
-                        </li>
+                        {/*<li className="nav-item">*/}
+                        {/*    <Link className={location.pathname === "/courses" ? 'nav-link' : 'nav-link-disable'} to="/courses">Courses</Link>*/}
+                        {/*</li>*/}
 
-                        <li className="nav-item">
-                            <Link className={location.pathname === "/course-content-categories" ? 'nav-link' : 'nav-link-disable'} to="/course-content-categories">Course Chapters</Link>
-                        </li>
+                        {/*<li className="nav-item">*/}
+                        {/*    <Link className={location.pathname === "/course-content-categories" ? 'nav-link' : 'nav-link-disable'} to="/course-content-categories">Course Chapters</Link>*/}
+                        {/*</li>*/}
 
-                        <li className="nav-item">
-                            <Link className={location.pathname === "/course-content" ? 'nav-link' : 'nav-link-disable'} to="/course-content">Chapter Content </Link>
-                        </li>
+                        {/*<li className="nav-item">*/}
+                        {/*    <Link className={location.pathname === "/course-content" ? 'nav-link' : 'nav-link-disable'} to="/course-content">Chapter Content </Link>*/}
+                        {/*</li>*/}
 
-                        <li className="nav-item">
-                            <Link className={location.pathname === "/course-question" ? 'nav-link' : 'nav-link-disable'} to="/course-question">Course Questions</Link>
-                        </li>
+                        {/*<li className="nav-item">*/}
+                        {/*    <Link className={location.pathname === "/course-question" ? 'nav-link' : 'nav-link-disable'} to="/course-question">Course Questions</Link>*/}
+                        {/*</li>*/}
 
 
                         {/*<li className="nav-item">*/}
@@ -48,17 +48,17 @@ const Main = () => {
                             <Link className={location.pathname === "/coding-challenge-list" ? 'nav-link' : 'nav-link-disable'} to="/coding-challenge-list">Coding Challenge </Link>
                         </li>
 
-                        <li className="nav-item">
-                            <Link className={location.pathname === "/quizs" ? 'nav-link' : 'nav-link-disable'} to="/quizs">Quiz</Link>
-                        </li>
+                        {/*<li className="nav-item">*/}
+                        {/*    <Link className={location.pathname === "/quizs" ? 'nav-link' : 'nav-link-disable'} to="/quizs">Quiz</Link>*/}
+                        {/*</li>*/}
 
-                        <li className="nav-item">
-                            <Link className={location.pathname === "/quiz-questions" ? 'nav-link' : 'nav-link-disable'} to="/quiz-questions">Quiz Questions</Link>
-                        </li>
+                        {/*<li className="nav-item">*/}
+                        {/*    <Link className={location.pathname === "/quiz-questions" ? 'nav-link' : 'nav-link-disable'} to="/quiz-questions">Quiz Questions</Link>*/}
+                        {/*</li>*/}
 
-                        <li className="nav-item">
-                            <Link className={location.pathname === "/instructors" ? 'nav-link' : 'nav-link-disable'} to="/instructors">Course Instructor</Link>
-                        </li>
+                        {/*<li className="nav-item">*/}
+                        {/*    <Link className={location.pathname === "/instructors" ? 'nav-link' : 'nav-link-disable'} to="/instructors">Course Instructor</Link>*/}
+                        {/*</li>*/}
 
                         <li className="nav-item">
                             <Link className={location.pathname === "/sliders" ? 'nav-link' : 'nav-link-disable'} to="/sliders">Sliders</Link>
